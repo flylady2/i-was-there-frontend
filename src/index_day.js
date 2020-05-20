74aca68da0e7a6fbcb81f375f302d2813cde3178
Leslie Stevens <flymaven@protonmail.com>
2020-05-20 16:23:07 -0500

    code to display day and entry content

--- src/index_day.js
+++ src/index_day.js
@@ -19,7 +19,7 @@ function getDays() {
 }
 
 function renderDay(day) {
-  console.log(day)
+  //console.log(day)
 
   let daysCollection = document.getElementById("days-container")
   const div = document.createElement('div')
@@ -29,29 +29,60 @@ function renderDay(day) {
     p1.innerText = `${day.attributes.name}`
   const p2 = document.createElement('p2')
     p2.innerText = `${day.attributes.date}`
-
+    day.attributes.entries.forEach(entry =>
+      renderEntry(entry))
     div.append(p1, p2)
     daysCollection.appendChild(div)
-
-
 }
 
 function renderEntry(entry) {
-  console.log(entry)
+  //console.log(entry)
 
-  let entriesCollection = document.getElementById("entries-collection")
+  let entriesCollection = document.getElementById("entries-container")
   const div = document.createElement('div')
     div.className = 'card'
     div.setAttribute("data-id", `${entry.id}`)
-  const p = document.createElement('p')
-    p.innerText = `${entry.attributes.content}`
-  const h2 = document.createElement('h2')
-    h2.innerHTML = `${entry.attributes.category.name}`
-  const h3 = document.createElement('h3')
-    h3.innerHTML = `${entry.attributes.day.date}`
-    let ul = document.createElement('ul')
-    div.append(p, h2, h3)
+    const h3 = document.createElement('h3')
+    h3.innerText = `${entry.content}`
+    //console.log(h3.innerText)
+  //const p = document.createElement('p')
+    //p.innerText = `${entry.attributes.content}`
+  //const h2 = document.createElement('h2')
+    //h2.innerHTML = `${entry.attributes.category.name}`
+  //const h3 = document.createElement('h3')
+    //h3.innerHTML = `${entry.attributes.day.date}`
+    //let ul = document.createElement('ul')
+    div.append(h3)
     entriesCollection.appendChild(div)
 
 
+}
+function createFormHandler(e) {
+  e.preventDefault()
+  const dateInput = document.querySelector('#entry-date').value
+  const nameInput = document.querySelector(`#input-name`).value
+  //const imageInput = document.querySelector('#input-path').value
+  //const categoryInput = document.querySelector('#categories').value
+  //const categoryId = parseInt(categoryInput)
+  //debugger
+  createDay(dateInput, nameInput)
+}
+
+function createDay(date, name) {
+  console.log(date, name)
+  let bodyData = {date, name}
+
+  fetch(endPoint, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json:"
+    },
+    body:JSON.stringify(bodyData)
+  })
+  .then(response => response.json())
+  .then(day => {
+    console.log(day);
+    renderDay(day)
+  })
 }
