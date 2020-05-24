aa254db66b2887cf2cedff373566c962231454ca
Leslie Stevens <flymaven@protonmail.com>
2020-05-24 15:31:04 -0500

    code to display day with entries

--- src/index_day.js
+++ src/index_day.js
@@ -10,13 +10,20 @@ document.addEventListener('DOMContentLoaded', () => {
 function getDays() {
   fetch(endPoint)
     .then(res => res.json())
-    //.then(json => console.log(json))
-    .then(days => {
-      //console.log(days)
-      days.data.forEach(day =>
 
+    .then(days => {
+      console.log(days)
+
+      const daysData = days.data
+      daysData.forEach(day =>
       renderDay(day))
+
+      const daysEntries = days.included
+      daysEntries.forEach(entry =>
+      renderEntry(entry))
+
       })
+
 
 }
 
@@ -41,21 +48,22 @@ function renderDay(day) {
     p1.innerText = `${day.attributes.name}`
   const p2 = document.createElement('p2')
     p2.innerText = `${day.attributes.date}`
-    day.attributes.entries.forEach(entry =>
-      renderEntry(entry))
+
     div.append(p1, p2)
     daysCollection.appendChild(div)
 }
 
 function renderEntry(entry) {
-  //console.log(entry)
+  console.log(entry)
 
   let entriesCollection = document.getElementById("entries-container")
   const div = document.createElement('div')
     div.className = 'card'
     div.setAttribute("data-id", `${entry.id}`)
+    const h2 = document.createElement('h2')
+    h2.innerText = `${entry.attributes.content}`
     const h3 = document.createElement('h3')
-    h3.innerText = `${entry.content}`
+    h3.innerText = `${entry.attributes.category.name}`
     //console.log(h3.innerText)
   //const p = document.createElement('p')
     //p.innerText = `${entry.attributes.content}`
@@ -64,7 +72,7 @@ function renderEntry(entry) {
   //const h3 = document.createElement('h3')
     //h3.innerHTML = `${entry.attributes.day.date}`
     //let ul = document.createElement('ul')
-    div.append(h3)
+    div.append(h2,h3)
     entriesCollection.appendChild(div)
 
 
