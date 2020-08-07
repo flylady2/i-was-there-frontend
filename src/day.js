43aa3f4426713d82b05ee2d48520b5ca51bfdf47
Leslie Stevens <flymaven@protonmail.com>
2020-08-06 19:16:46 -0500

    remove addBtn code, add code for reload and reloadBtn

--- src/day.js
+++ src/day.js
@@ -1,7 +1,6 @@
 class Day {
 
   constructor(day) {
-    console.log('here')
     this.id = day.id
     this.name = day.attributes.name
     this.date = day.attributes.date
@@ -24,8 +23,17 @@ class Day {
       p1.innerText = `${this.name}`
     const p2 = document.getElementById('right-header')
       p2.innerText = `${this.date}`
+
     const addBtn = document.querySelector('#new-day-btn')
-      addBtn.style.visibility = "visible"
+    addBtn.style.visibility = "hidden"
+    const reloadP = document.querySelector('#reload')
+    reloadP.style.visibility = "visible"
+    const reloadBtn = document.querySelector('#reload-btn')
+    reloadBtn.style.visibility = "visible"
+
+    reloadBtn.addEventListener('click', () => {
+      location.reload()  
+    })
 
   }
 }
